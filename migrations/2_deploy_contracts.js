89276ed6346520ec2c8edbcf2b62360f150e41da
Tomás Bustamante <tbustamante@atixlabs.com>
2020-08-05 20:44:41 -0300

    feat: agregar CryptoTienda en el script de migracion

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const CryptoTienda = artifacts.require("./CryptoTienda.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(CryptoTienda);
 };
