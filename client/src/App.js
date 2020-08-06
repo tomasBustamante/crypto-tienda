20d99f0c6713fcf9d0d16a1df40804a6d98c55df
Tomás Bustamante <tbustamante@atixlabs.com>
2020-08-06 01:14:18 -0300

    refactor: paso el componente App como funcion con hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,114 +1,101 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 import "./App.css";
 import CryptoTienda from "./contracts/CryptoTienda.json";
 import Navbar from "./Navbar";
 import Main from "./Main";
 
-class App extends Component {
-  async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState("");
+  const [contrato, setContrato] = useState();
+  const [productos, setProductos] = useState([]);
 
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider);
-    } else {
-      window.alert(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3;
-    // Load account
-    const accounts = await web3.eth.getAccounts();
-    this.setState({ account: accounts[0] });
-    const networkId = await web3.eth.net.getId();
-    const networkData = CryptoTienda.networks[networkId];
-    if (networkData) {
-      const cryptoTienda = new web3.eth.Contract(
-        CryptoTienda.abi,
-        networkData.address
-      );
-      this.setState({ cryptoTienda });
-      const cantidadProductos = await cryptoTienda.methods.cantidadProductos().call();
-      this.setState({ cantidadProductos });
-      // Load productos
-      for (var i = 1; i <= cantidadProductos; i++) {
-        const product = await cryptoTienda.methods.productos(i).call();
-        this.setState({
-          productos: [...this.state.productos, product],
-        });
+  useEffect(() => {
+    const loadWeb3 = async () => {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        window.alert(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
       }
-      this.setState({ loading: false });
-    } else {
-      window.alert("CryptoTienda contract not deployed to detected network.");
-    }
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: "",
-      cantidadProductos: 0,
-      productos: [],
-      loading: true,
     };
+    const loadBlockchainData = async () => {
+      const web3 = window.web3;
+      // Cargar account
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      const networkId = await web3.eth.net.getId();
+      const networkData = CryptoTienda.networks[networkId];
+      if (networkData) {
+        const cryptoTienda = new web3.eth.Contract(
+          CryptoTienda.abi,
+          networkData.address
+        );
+        setContrato(cryptoTienda);
+        const cantidadProductos = await cryptoTienda.methods
+          .cantidadProductos()
+          .call();
+        // Cargar productos
+        for (let i = 1; i <= cantidadProductos; i++) {
+          const producto = await cryptoTienda.methods.productos(i).call();
+          setProductos([...productos, producto]);
+        }
+        setLoading(false);
+      } else {
+        window.alert("CryptoTienda contract not deployed to detected network.");
+      }
+    };
+    loadWeb3();
+    loadBlockchainData();
+  });
 
-    this.crearProducto = this.crearProducto.bind(this);
-    this.comprarProducto = this.comprarProducto.bind(this);
-  }
-
-  crearProducto(name, price) {
-    this.setState({ loading: true });
-    this.state.cryptoTienda.methods
-      .crearProducto(name, price)
-      .send({ from: this.state.account })
+  const crearProducto = (nombre, precio) => {
+    setLoading(true);
+    contrato.methods
+      .crearProducto(nombre, precio)
+      .send({ from: account })
       .once("receipt", (receipt) => {
-        this.setState({ loading: false });
+        setLoading(false);
       });
-  }
+  };
 
-  comprarProducto(id, price) {
-    this.setState({ loading: true });
-    this.state.cryptoTienda.methods
+  const comprarProducto = (id, precio) => {
+    setLoading(true);
+    contrato.methods
       .comprarProducto(id)
-      .send({ from: this.state.account, value: price })
+      .send({ from: account, value: precio })
       .once("receipt", (receipt) => {
-        this.setState({ loading: false });
+        setLoading(false);
       });
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex">
-              {this.state.loading ? (
-                <div id="loader" className="text-center">
-                  <p className="text-center">Loading...</p>
-                </div>
-              ) : (
-                <Main
-                  productos={this.state.productos}
-                  crearProducto={this.crearProducto}
-                  comprarProducto={this.comprarProducto}
-                />
-              )}
-            </main>
-          </div>
+  return (
+    <div>
+      <Navbar account={account} />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex">
+            {loading ? (
+              <div id="loader" className="text-center">
+                <p className="text-center">Loading...</p>
+              </div>
+            ) : (
+              <Main
+                productos={productos}
+                crearProducto={crearProducto}
+                comprarProducto={comprarProducto}
+              />
+            )}
+          </main>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
